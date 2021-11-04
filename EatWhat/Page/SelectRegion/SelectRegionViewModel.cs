da3c852d6ef8c12c737be74c0d6f6548e3594dcb
CalvinChan <lcpscalvin@gmail.com>
2021-11-04 14:49:12 +0800

    Add SelectRegion Page

---
+++ EatWhat/Page/SelectRegion/SelectRegionViewModel.cs
@@ -0,0 +1,16 @@
+﻿using EatWhat.Common;
+using EatWhat.Framework;
+using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace EatWhat.Page.SelectRegion
+{
+    public class SelectRegionViewModel : BaseViewModel
+    {
+        public SelectRegionViewModel()
+        {
+           
+        }
+    }
+}
